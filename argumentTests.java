a78a6de27944c777ee18845993e28365c2be3616
Alex Teeter <alteeter@gmail.com>
2015-09-25 14:07:47 -0500

    Added all of my files, unittest.java is closest to 'real' file

---
+++ argumentTests.java
@@ -0,0 +1,20 @@
+package software-project;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class argumentTests {
+        @Before
+        ParseArgs p = new ParseArgs();
+        @Test(expected = IllegalArgumentException.class)
+        public void testForNotEnoughArguments{
+                assertEquals(map.length, 0);
+                p.addArg("length");
+                assertEquals(map.length, 1);
+                p.addArg("width");
+                assertEquals(map.length, 2);
+                p.addArg("height");
+                assertEquals(map.length, 3);
+                p.argsGiven.add("0");
+                //return p.argsGiven to main         
+        }
