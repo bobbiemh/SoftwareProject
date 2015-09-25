95627cc6c07f9d70d3eb558d97b1980ff2a704ad
batmantotoro <bhoskins@stu.jsu.edu>
2015-09-25 13:10:40 -0500

    ParseArgs
    error in finding class

---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+import SoftwareProject.ParseArgs;
+import java.util.*;
+
+public class main {
+
+
+	public static void main(String[] args)
+	{
+		ParseArgs p = new ParseArgs();
+		
+		p.addArg("Length");
+		p.addArg("Width");
+		p.addArg("Height");
+		//p.parse(args);
+	}
+}
---
+++ src/softwareproject/ParseArgs.java
@@ -0,0 +1,34 @@
+
+package softwareproject;
+
+import java.util.*;
+
+public class ParseArgs {
+    	//need a function to see in the args
+	//separate
+	//print out error if there's not enough or to many
+	
+	Map<String, Integer> map = new HashMap<String, Integer>();
+	
+	public void addArgs(String userInput)
+	{
+		map.put(userInput, 0);
+	}/*
+	public void parse(int[] args)
+	{
+		for(int i = 0; i < args.length; i++)
+		{
+			int temp = Integer.parseInt("9");
+		}
+		
+	}
+	public int getArgs(String whatUserWants)
+	{
+		return 0;
+	}
+	
+	public String printArgs()
+	{
+		return "0";
+	}*/
+}
