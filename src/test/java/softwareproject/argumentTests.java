70fecdb7c71f7ac3316065c5c1ff848b7b908545
batmantotoro <bhoskins@stu.jsu.edu>
2015-09-30 11:44:45 -0500

    because spaces

--- src/test/java/softwareproject/argumentTests.java
+++ src/test/java/softwareproject/argumentTests.java
@@ -13,25 +13,25 @@ public class argumentTests {
         
         @Test
         public void enoughArguments() {
-                assertEquals(p.getNumberOfKeys(), 0);
-                p.addArgs("length");
-                assertEquals(p.getNumberOfKeys(), 1);
-                p.addArgs("width");
-                assertEquals(p.getNumberOfKeys(), 2);
-                p.addArgs("height");
-                assertEquals(p.getNumberOfKeys(), 3);
-                String[] args = {"0", "0", "0"};
-                p.parse(args);
-                assertEquals(p.getNumberOfArgs(args), 3);
+			assertEquals(p.getNumberOfKeys(), 0);
+			p.addArgs("length");
+			assertEquals(p.getNumberOfKeys(), 1);
+			p.addArgs("width");
+			assertEquals(p.getNumberOfKeys(), 2);
+			p.addArgs("height");
+			assertEquals(p.getNumberOfKeys(), 3);
+			String[] args = {"0", "0", "0"};
+			p.parse(args);
+			assertEquals(p.getNumberOfArgs(args), 3);
         }
         @Test
 		public void TooFewArgs(){
-				p.addArgs("length");
-				p.addArgs("width");
-				assertEquals(p.getNumberOfKeys(), 2);
-				String[] args = {"0","0","0"};
-				p.parse(args);
-				assertEquals(p.getNumberOfArgs(args), 3);
+			p.addArgs("length");
+			p.addArgs("width");
+			assertEquals(p.getNumberOfKeys(), 2);
+			String[] args = {"0","0","0"};
+			p.parse(args);
+			assertEquals(p.getNumberOfArgs(args), 3);
 		}
 		
 		@Test
